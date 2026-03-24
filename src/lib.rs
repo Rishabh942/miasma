648c6d815aca0f17ad2b227a33f1fe481eca1e03
austin-weeks <168379071+austin-weeks@users.noreply.github.com>
2026-03-23 20:19:47 -0400

    organize module structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
 mod config;
-pub mod routes;
+mod routes;
 
-pub use config::CONFIG;
+use std::sync::Arc;
+
+use axum::{Router, routing::get};
+pub use config::MiasmaConfig;
+use tokio::sync::Semaphore;
+
+/// Build a new `axum::Router` for miasma's route.
+pub fn new_miasma_router(config: &'static MiasmaConfig) -> Router {
+    let in_flight_sem = Arc::new(Semaphore::new(config.max_in_flight as usize));
+
+    Router::new().fallback(get(move || routes::serve_poison(config, in_flight_sem)))
+}
